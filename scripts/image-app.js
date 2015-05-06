1c3784e8fd927ec767f03bef12d270487cf3d1eb
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-05-05 21:41:38 -0500

    fix(image-app):use of worker in manipulateImage-lines 8,42,43

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -5,7 +5,7 @@
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
-
+  var worker = new Worker('scripts/worker.js');
   function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
@@ -28,7 +28,8 @@
     for (var i = 0; i < buttons.length; i++) {
       if (buttons[i].hasAttribute('disabled')) {
         buttons[i].removeAttribute('disabled')
-      } else {
+      }
+      else {
         buttons[i].setAttribute('disabled', null);
       }
     };
@@ -37,26 +38,18 @@
   function manipulateImage(type) {
     var a, b, g, i, imageData, j, length, pixel, r, ref;
     imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-    toggleButtonsAbledness();
+    //toggleButtonsAbledness();
+    worker.postMessage({'imageData': imageData, 'type': type});
+    worker.onmessage=function(e){
+      toggleButtonsAbledness();
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+      var image = e.data;
+      if(image) return ctx.putImageData(e.data,0,0);
+      console.log("No manipulated image.");
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
   };
 
   function revertImage() {
